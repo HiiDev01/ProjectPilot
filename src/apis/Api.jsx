aa6d7e13a06f116caf95df301a6b4b7694122fee
hiidev <cadetolati@gmail.com>
2025-08-18 10:54:36 -1100

    dashboard updated

---
+++ src/apis/Api.jsx
@@ -0,0 +1,21 @@
+const API_URL = "http://localhost:5000";
+
+export const getClients =  async() => {
+  const res = await fetch(`${API_URL}/clients`);
+  return await res.json();
+}
+
+export const getProjects = async () =>{
+  const res = await fetch(`${API_URL}/projects`);
+  return await res.json();
+}
+
+export const getPendings = async () =>{
+  const res = await fetch(`${API_URL}/projects?status=pending}`);
+  return await res.json();
+}
+export const getDelivered = async () =>{
+  const res = await fetch(`${API_URL}/projects?status=delivered}`);
+  return await res.json();
+}
+
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,15 +1,91 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import NavBar from '../components/NavBar'
 import '../styles/Dashboard.css'
+import { LuUsers,LuClock } from "react-icons/lu";
+import { BsSuitcaseLg } from "react-icons/bs";
+import { IoMdCheckmarkCircleOutline } from "react-icons/io";
+import { getClients, getProjects,getPendings,getDelivered } from '../apis/Api'
+
 
 const Dashboard = () => {
+  const [dashItem, setDashItem] = useState({
+    totalClient: 10,
+    totalProject: 8,
+    pendingProject: 2,
+    deliveredProjects: 12
+  })
+
+  useEffect(()=>{
+    const fetchItems = async() =>{
+      try {
+        const [client, projects, pending, delivered] = await Promise.all([
+          getClients(),
+          getProjects(),
+          getPendings(),
+          getDelivered()
+        ]);
+        setDashItem({
+          totalClient: client.length,
+          totalProject: projects.length,
+          pendingProject: pending.length,
+          deliveredProjects: delivered.length
+        });
+      
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchItems();
+  },[])
+  
   return (
     <div className='dashboard'>
       <>
         <NavBar/>
       </>
       <div className='main'>
-       
+        <div className='head'>
+          <h1>Dashboard</h1>
+          <p>A quick overview of your business.</p>
+        </div>
+        <div className="dashGrid">
+          <div className='dashItem'>
+            <div className='heading'>
+              <p>total client</p>
+              <LuUsers size={18} className='dashIcon'/>
+            </div>
+            <h1>{dashItem.totalClient}</h1>
+            <p>+2 last month</p>
+          </div>
+
+          <div className='dashItem'>
+            <div className='heading'>
+              <p>total projects</p>
+              <BsSuitcaseLg size={18} className='dashIcon'/>
+            </div>
+            <h1>{dashItem.totalProject}</h1>
+            <p>+2 last month</p>
+          </div>
+
+          <div className='dashItem'>
+            <div className='heading'>
+              <p>pending projects</p>
+              <LuClock size={18} className='dashIcon'/>
+            </div>
+            <h1>{dashItem.pendingProject}</h1>
+            <p>Awaiting approval</p>
+          </div>
+
+          <div className='dashItem'>
+            <div className='heading'>
+              <p>delivered projects</p>
+              <IoMdCheckmarkCircleOutline size={18} className='dashIcon'/>
+            </div>
+            <h1>{dashItem.deliveredProjects}</h1>
+            <p>Completed successfully</p>
+          </div>
+
+        </div>
       </div>
     </div>
   )
