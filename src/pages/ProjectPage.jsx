4e3b6d4d90a324f5489c0a0d4f95786ca979cf83
hiidev <cadetolati@gmail.com>
2025-08-22 03:37:28 -1100

    update

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
 import '../styles/ProjectPage.css'
+import { IoIosAddCircleOutline } from "react-icons/io";
 
 const ProjectPage = () => {
   return (
@@ -14,6 +15,23 @@ const ProjectPage = () => {
             <h1>All Projects</h1>
             <p>Manage your projects here. An overview of all  latest projects.</p>
           </div>
+          <button className='addProjectBtn'> 
+            <span><IoIosAddCircleOutline size={18}/></span>
+            add project
+          </button>
+        </div>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>project</th>
+                <th>client</th>
+                <th>status</th>
+                <th>dues date</th>
+                <th>action</th>
+              </tr>
+            </thead>
+          </table>
         </div>
       </main>
     </div>
